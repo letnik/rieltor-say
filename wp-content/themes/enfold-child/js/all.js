650a77d8df7dd250f2adc71a5b8990297076fc8e
Ostap Letnyanchyk <ostap.letnyanchyk@gmail.com>
2021-02-10 17:52:54 +0200

    map start

--- wp-content/themes/enfold-child/js/all.js
+++ wp-content/themes/enfold-child/js/all.js
@@ -1,17 +1,17 @@
 jQuery(document).ready(function () {
 
-	jQuery('.geodir-filter-cat').on('click', function() {
+	jQuery('.geodir-filter-cat:not(.gd-field-post_category)').on('click', function() {
 
 		if ( jQuery(this).hasClass('is-open') ){
 
 		  } else {
 			
-			jQuery('.geodir-filter-cat').find('span').removeClass('is-open');
+			jQuery('.geodir-filter-cat:not(.gd-field-post_category)').find('span').removeClass('is-open');
 			jQuery(this).find('span').addClass('is-open');
 			
-			jQuery('.geodir-filter-cat').removeClass('is-open');
+			jQuery('.geodir-filter-cat:not(.gd-field-post_category)').removeClass('is-open');
 			jQuery(this).addClass('is-open');
-			jQuery('.geodir-filter-cat').find('ul').fadeOut(200);
+			jQuery('.geodir-filter-cat:not(.gd-field-post_category)').find('ul').fadeOut(200);
 			jQuery(this).find('ul').fadeIn(200);
 			jQuery('.trigger-filter').fadeIn(200);
 	
@@ -21,9 +21,9 @@ jQuery(document).ready(function () {
 
 	jQuery('.trigger-filter').on('click', function() {
 
-		jQuery('.geodir-filter-cat').find('span').removeClass('is-open');	
-		jQuery('.geodir-filter-cat').removeClass('is-open');	
-		jQuery('.geodir-filter-cat').find('ul').fadeOut(200);
+		jQuery('.geodir-filter-cat:not(.gd-field-post_category)').find('span').removeClass('is-open');	
+		jQuery('.geodir-filter-cat:not(.gd-field-post_category)').removeClass('is-open');	
+		jQuery('.geodir-filter-cat:not(.gd-field-post_category)').find('ul').fadeOut(200);
 		jQuery('.trigger-filter').fadeOut(200);
 
 	});
@@ -35,13 +35,16 @@ jQuery(document).ready(function () {
 	jQuery('.geodir-filter-cat li input[type=checkbox]').each(function () {
 		if (jQuery(this).prop('checked')) {
 			jQuery(this).parent().addClass( "checked" );
+
 		} else {
 			jQuery(this).parent().removeClass( "checked" );
+
 		}
 	});
 
 	jQuery(":checkbox").change(function () {
 		jQuery(this).parent().toggleClass('checked');
+
 	});
 	
 });
